7a07b485d85543e64ac735c7cdcf941a0630a692
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-03-27 22:18:30 -0500

    Added express-session; created session config object

--- index.js
+++ index.js
@@ -2,15 +2,29 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-modal.js');
 
 const server = express();
 
+const sessionConfig = {
+  name: 'notsession', // default is connect.sid
+  secret: 'nobody tosses a dwarf!',
+  cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+    secure: false, // only set cookies over https. Server will not send back a cookie over http.
+  }, // 1 day in milliseconds
+  httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+  resave: false,
+  saveUninitialized: false,
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.post('/api/register', (req, res) => {
   let newUser = req.body;
